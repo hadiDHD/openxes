77c3427c7b9d34b4e9c8ee3ce008ec3015369852
bfvdonge <bfvdonge@a68f48d4-8d00-424f-b812-3f22a534a02a>
2011-09-19 08:44:02 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/OpenXES/trunk@3118 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/deckfour/xes/nikefs2/NikeFS2VirtualFileSystem.java
+++ src/org/deckfour/xes/nikefs2/NikeFS2VirtualFileSystem.java
@@ -47,162 +47,167 @@ import org.deckfour.xes.logging.XLogging;
 import org.deckfour.xes.logging.XLogging.Importance;
 
 /**
- * Virtual file system authority, managing swap files and
- * virtual file abstractions. Implements the storage provider
- * interface for use by file abstractions, and performs
- * cleanup after a shutdown of the host application.
+ * Virtual file system authority, managing swap files and virtual file
+ * abstractions. Implements the storage provider interface for use by file
+ * abstractions, and performs cleanup after a shutdown of the host application.
  * 
- * This class must be used as a singleton. There is no need
- * for multiple virtual file systems per application. A single
- * VFS instance operates with maximum efficiency, and guarantees
- * that no data corruption or swap loss will occur.
+ * This class must be used as a singleton. There is no need for multiple virtual
+ * file systems per application. A single VFS instance operates with maximum
+ * efficiency, and guarantees that no data corruption or swap loss will occur.
  * 
  * @author Christian W. Guenther (christian@deckfour.org)
- *
+ * 
  */
 public class NikeFS2VirtualFileSystem implements NikeFS2StorageProvider {
-	
+
 	/**
 	 * The singleton VFS instance.
 	 */
 	protected static NikeFS2VirtualFileSystem instance = null;
-	
+
 	/**
 	 * Singleton access method.
 	 * 
 	 * @return The singleton instance.
 	 */
 	public synchronized static NikeFS2VirtualFileSystem instance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new NikeFS2VirtualFileSystem();
 		}
 		return instance;
 	}
-	
+
 	/**
-	 * Block size, in bytes, of virtual storage blocks.
-	 * Default value is 2048, i.e. 2kB.
+	 * Block size, in bytes, of virtual storage blocks. Default value is 2048,
+	 * i.e. 2kB.
 	 */
 	protected int blockSize = 2048;
 	/**
-	 * Size of swap files which are used to provide blocks.
-	 * Default is 67108864, i.e. 64 MB.
+	 * Size of swap files which are used to provide blocks. Default is 67108864,
+	 * i.e. 64 MB.
 	 */
 	protected int swapFileSize = 67108864;
 	/**
-	 * Flag determining whether to use soft copies for
-	 * virtual file duplication. This greatly enhances 
-	 * performance with negligible memory penalties, thus
+	 * Flag determining whether to use soft copies for virtual file duplication.
+	 * This greatly enhances performance with negligible memory penalties, thus
 	 * the default value is <code>true</code>.
 	 */
 	protected boolean useLazyCopies = true;
 	/**
-	 * List of block providers, i.e. currently used swap
-	 * files that serve for providing storage blocks to
-	 * virtual file abstractions.
+	 * List of block providers, i.e. currently used swap files that serve for
+	 * providing storage blocks to virtual file abstractions.
 	 */
 	protected List<NikeFS2BlockProvider> blockProviders;
-	
+
 	/**
-	 * Creates a new virtual file system instance.
-	 * (Hidden private constructor, use singleton
-	 * accessor method!)
+	 * Creates a new virtual file system instance. (Hidden private constructor,
+	 * use singleton accessor method!)
 	 */
 	private NikeFS2VirtualFileSystem() {
 		blockProviders = new ArrayList<NikeFS2BlockProvider>();
 	}
-	
+
 	/**
-	 * Sets this VFS's property of whether to use soft copies
-	 * for virtual file duplication. This will only affect newly
-	 * created virtual files.
+	 * Sets this VFS's property of whether to use soft copies for virtual file
+	 * duplication. This will only affect newly created virtual files.
 	 * 
-	 * @param useLazyCopies Whether to use soft copies
-	 * for virtual file duplication.
+	 * @param useLazyCopies
+	 *            Whether to use soft copies for virtual file duplication.
 	 */
 	public synchronized void setUseLazyCopies(boolean useLazyCopies) {
 		this.useLazyCopies = useLazyCopies;
 	}
-	
+
 	/**
-	 * Sets the swap file size of the virtual file system.
-	 * (used henceforth until successive change).
+	 * Sets the swap file size of the virtual file system. (used henceforth
+	 * until successive change).
 	 * 
-	 * @param bytes Size of swap files, in bytes.
+	 * @param bytes
+	 *            Size of swap files, in bytes.
 	 */
 	public synchronized void setSwapFileSize(int bytes) {
 		this.swapFileSize = bytes;
 	}
-	
+
 	/**
-	 * Sets the block size of the virtual file system.
-	 * (used henceforth until successive change).
+	 * Sets the block size of the virtual file system. (used henceforth until
+	 * successive change).
 	 * 
-	 * @param bytes Size of blocks, in bytes.
+	 * @param bytes
+	 *            Size of blocks, in bytes.
 	 */
 	public synchronized void setBlockSize(int bytes) {
 		this.blockSize = bytes;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.processmining.framework.log.rfb.io.StorageProvider#createStorage()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.processmining.framework.log.rfb.io.StorageProvider#createStorage()
 	 */
 	public NikeFS2RandomAccessStorage createStorage() throws IOException {
-		if(useLazyCopies == true) {
+		if (useLazyCopies == true) {
 			return new NikeFS2LazyRandomAccessStorageImpl(this);
 		} else {
 			return new NikeFS2RandomAccessStorageImpl(this);
 		}
 	}
-	
+
 	/**
 	 * Returns the block size of this VFS.
 	 * 
-	 * @return The size of virtual storage blocks served by
-	 * this VFS, in bytes.
+	 * @return The size of virtual storage blocks served by this VFS, in bytes.
 	 */
 	public int blockSize() {
 		return blockSize;
 	}
-	
+
 	/**
-	 * Allocates a new virtual storage block from this virtual file
-	 * system instance. If no currently allocated swap file can provide
-	 * any more storage blocks, a new swap file will be allocated.
+	 * Allocates a new virtual storage block from this virtual file system
+	 * instance. If no currently allocated swap file can provide any more
+	 * storage blocks, a new swap file will be allocated.
 	 * 
 	 * @return An empty storage block abstraction.
 	 */
-	public synchronized NikeFS2Block allocateBlock() 
-			throws IOException {
-		// try to allocate from already created providers first
-		for(NikeFS2BlockProvider provider : blockProviders) {
-			if(provider.numberOfFreeBlocks() > 0) {
-				NikeFS2Block block = provider.allocateBlock();
-				if(block != null) {
-					return block;
+	public NikeFS2Block allocateBlock() throws IOException {
+		synchronized (this) {
+			// try to allocate from already created providers first
+			for (NikeFS2BlockProvider provider : blockProviders) {
+				if (provider.numberOfFreeBlocks() > 0) {
+					NikeFS2Block block = provider.allocateBlock();
+					if (block != null) {
+						return block;
+					}
 				}
 			}
 		}
 		// force garbage collection and try again (stale files still around?)
+		// Running the finalization might cause the Finalizer thread to call the allocateBlock method again,
+		// hence the lock on "this" should be removed before calling System.runFinalization
 		System.gc();
 		System.runFinalization();
 		Thread.yield();
-		for(NikeFS2BlockProvider provider : blockProviders) {
-			if(provider.numberOfFreeBlocks() > 0) {
-				NikeFS2Block block = provider.allocateBlock();
-				if(block != null) {
-					return block;
+		synchronized (this) {
+			for (NikeFS2BlockProvider provider : blockProviders) {
+				if (provider.numberOfFreeBlocks() > 0) {
+					NikeFS2Block block = provider.allocateBlock();
+					if (block != null) {
+						return block;
+					}
 				}
 			}
+			// ok, we give up:
+			// create new swap file and provider, and allocate from there
+			XLogging.log("NikeFS2: Allocating new swap file. (#"
+					+ (blockProviders.size() + 1) + ": " + swapFileSize
+					+ " bytes)", Importance.DEBUG);
+			File swapFile = NikeFS2SwapFileManager.createSwapFile();
+			NikeFS2BlockProvider addedProvider = new NikeFS2BlockProvider(
+					swapFile, swapFileSize, blockSize, true);
+			blockProviders.add(addedProvider);
+			return addedProvider.allocateBlock();
 		}
-		// ok, we give up:
-		// create new swap file and provider, and allocate from there
-		XLogging.log("NikeFS2: Allocating new swap file. (#" + (blockProviders.size() + 1) + ": " + swapFileSize + " bytes)", Importance.DEBUG);
-		File swapFile = NikeFS2SwapFileManager.createSwapFile();
-		NikeFS2BlockProvider addedProvider = new NikeFS2BlockProvider(swapFile, swapFileSize, blockSize, true);
-		blockProviders.add(addedProvider);
-		return addedProvider.allocateBlock();
 	}
-	
 
 }
