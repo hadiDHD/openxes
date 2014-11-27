54a7a56602d1e4363c8a7575fae32619860d4475
hverbeek <hverbeek@a68f48d4-8d00-424f-b812-3f22a534a02a>
2014-11-27 13:49:24 +0000

    Updated XES schema file and test. The schema file now requires (as was) that the log attributes follow the classifiers and precede the traces. Also fixed xmlns issue.
    git-svn-id: https://svn.win.tue.nl/repos/prom/OpenXES/trunk@17473 a68f48d4-8d00-424f-b812-3f22a534a02a

--- test/SchemaTest.java
+++ test/SchemaTest.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Locale;
 
 import javax.swing.JFileChooser;
@@ -26,13 +27,13 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class SchemaTest {
 
-	private void runTest() {
+	private void runHttpTest() {
 		try {
-        	URI uriXES = URI.create("http://www.xes-standard.org/xes21.xsd");
+        	URI uriXES = URI.create("http://www.xes-standard.org/xes22.xsd");
             BufferedInputStream isXES = new BufferedInputStream(uriXES.toURL().openStream());
             Source xesSchemaFile = new StreamSource(isXES);
 
-            URI uriEXT = URI.create("http://www.xes-standard.org/xesext21.xsd");
+            URI uriEXT = URI.create("http://www.xes-standard.org/xesext22.xsd");
             BufferedInputStream isEXT = new BufferedInputStream(uriEXT.toURL().openStream());
             Source extSchemaFile = new StreamSource(isEXT);             
 
@@ -58,13 +59,48 @@ public class SchemaTest {
 			
         } catch (SAXException e) {
                e.printStackTrace();
-
         } catch (MalformedURLException e) {
                e.printStackTrace();
-
         } catch (IOException e) {
-               e.printStackTrace();
+               e.printStackTrace();               
+        } catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private void runFileTest() {
+		try {
+            Source xesSchemaFile = new StreamSource(new File("documentation/xes22.xsd"));
+
+            Source extSchemaFile = new StreamSource(new File("documentation/xesext22.xsd"));
+
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);              
+
+            Schema schema = schemaFactory.newSchema(new Source[] {xesSchemaFile, extSchemaFile});
                
+		    SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+		    parserFactory.setNamespaceAware(true);
+		    parserFactory.setSchema(schema);
+
+		    SAXParser parser = parserFactory.newSAXParser();
+
+		    SimpleHandler handler = new SimpleHandler();
+			
+			JFileChooser fileChooser = new JFileChooser();
+	    	int choice = fileChooser.showDialog(null, "Select XES File");
+	    	if (choice == JFileChooser.APPROVE_OPTION) {    		
+	    		String absolutePath = fileChooser.getSelectedFile().getAbsolutePath();
+	    		parser.parse(new File(absolutePath), handler);
+	    	}	
+	    	
+			
+        } catch (SAXException e) {
+               e.printStackTrace();
+        } catch (MalformedURLException e) {
+               e.printStackTrace();
+        } catch (IOException e) {
+               e.printStackTrace();               
         } catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		}
@@ -97,7 +133,7 @@ public class SchemaTest {
     public static void main(String[] args) {
     	Locale.setDefault(Locale.UK);
     	SchemaTest validator = new SchemaTest();
-    	validator.runTest();
+    	validator.runFileTest();
     }
 
 }
